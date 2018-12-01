2f58f1c30245e0acf3e8603d8c9116157cc64176
prakashmehta@97 <prakash.mehta2912@gmail.com>
2018-12-01 10:34:14 +0530

    Commit From Rest branch

--- app/src/main/java/com/cbo/cbomobilereporting/ui_new/report_activities/FirstActivty.java
+++ app/src/main/java/com/cbo/cbomobilereporting/ui_new/report_activities/FirstActivty.java
@@ -1,4 +1,14 @@
 package com.cbo.cbomobilereporting.ui_new.report_activities;
 
 public class FirstActivty {
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
+
+
+
+
+
