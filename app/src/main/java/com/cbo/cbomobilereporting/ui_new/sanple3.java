15e541f8100ca6a590a8e19e6dc7952c409cad8a
prakashmehta@97 <prakash.mehta2912@gmail.com>
2019-01-07 13:32:57 +0530

    Test 1 add A code snippet from V3.00 brnach

--- app/src/main/java/com/cbo/cbomobilereporting/ui_new/sanple3.java
+++
@@ -1,4 +0,0 @@
-package com.cbo.cbomobilereporting.ui_new;
-
-public class sanple3 {
-}
---
+++ app/src/main/java/com/cbo/cbomobilereporting/ui_new/sanple4.java
@@ -0,0 +1,15 @@
+package com.cbo.cbomobilereporting.ui_new;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class sanple4  extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+    }
+}
+
+
